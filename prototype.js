b3c2ff4376a1a945adf0e0b948144e810cc483df
abiha456 <abiha.amir456@gmail.com>
2022-05-22 14:39:40 -0700

    protype

--- prototype.js
+++ prototype.js
@@ -2,13 +2,12 @@ let User = function (name, age, address) {
   this.name = name;
   this.age = age;
   this.address = address;
-
-  this.online = function () {
-    console.log(`this user ${this.name} is online`);
-  };
-  this.offline = function () {
-    console.log(`this user ${this.name} is offline`);
-  };
+};
+User.prototype.online = function () {
+  console.log(`this user ${this.name} is online`);
+};
+User.prototype.offline = function () {
+  console.log(`this user ${this.name} is offline`);
 };
 let user1 = new User("ali", 21, "karachi");
-console.log(user1.name)
+console.log(user1.online());
